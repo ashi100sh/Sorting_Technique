c1f24b0175a661d77fe1865b74589d1ebbfea9ee
Ashish Singh <ashi100sh@gmail.com>
2016-10-10 15:39:27 +0530

    Bubble Sort programme

---
+++ BubbleSort/bubbleSortOptimize.c
@@ -0,0 +1,85 @@
+/*
+ * Bubble Sort : Bubble Sort is based on the idea of repeatedly comparing pairs of adjacent elements
+ * and then switching their positions if they exist in the wrong order.
+ * Optimization: If array is already sorted then don't check comparing pairs of adjacent elements.
+ * In that case simply break from loop.
+ */
+#include <stdio.h>
+#include <stdbool.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubbleSort(int arr[], int size)
+{
+    int i, j;
+    bool sorted;
+    int no_of_swap = 0;
+
+    for(int i=0; i<size-1; i++)
+    {
+        bool sorted = true; 
+        for(int j=0; j<size-i-1; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                swap(&arr[j], &arr[j+1]);
+                sorted = false;
+		no_of_swap++;
+            } 
+        }
+        //If inner loop haven't perform any sorting , it means it is sorted array.
+        if(sorted == true)
+            break;
+    }
+    printf("No of swap: %d\n", no_of_swap);
+}
+
+int main()
+{
+    int arr1[] = {5, 10, 3, 2, 15, 7, 18, 1, 5};
+    int arr2[] = {1, 2, 3, 10, 20, 30, 40, 50};
+    int sizeArr1 = sizeof(arr1)/sizeof(int);
+    int sizeArr2 = sizeof(arr2)/sizeof(int);
+    printf("====================================\n");
+    printf("Before Sorting Array1:\n");
+    for(int i=0; i<sizeArr1; i++)
+    {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
+
+    //Calling bubble sort function
+    bubbleSort(arr1, sizeArr1);
+    
+    printf("After Sorting Array1:\n");
+    for(int i=0; i<sizeArr1; i++)
+    {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n\n");
+
+
+    printf("====================================\n");
+    printf("Before Sorting Array2:\n");
+    for(int i=0; i<sizeArr2; i++)
+    {
+        printf("%d ", arr2[i]);
+    }
+    printf("\n");
+
+    //Calling bubble sort function
+    bubbleSort(arr2, sizeArr2);
+    
+    printf("After Sorting Array2:\n");
+    for(int i=0; i<sizeArr2; i++)
+    {
+        printf("%d ", arr2[i]);
+    }
+    printf("\n\n");
+}
+   
